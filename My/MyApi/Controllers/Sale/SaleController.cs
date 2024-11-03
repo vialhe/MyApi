db4169b0b8c7db56a6603bdbeb4b1cdad130d564
Victor7u7 <107167883+alfonsouwu@users.noreply.github.com>
2024-11-03 13:54:41 -0600

    Se mejora estructura de EP para insertar ventas
    Se mejora estructura de EP para insertar ventas

--- My/MyApi/Controllers/Sale/SaleController.cs
+++ My/MyApi/Controllers/Sale/SaleController.cs
@@ -13,43 +13,35 @@ namespace MyApi.Controllers.Sale
     [Route("[controller]")]
     public class SaleController : ControllerBase
     {
-
-
         [HttpPost]
         [Route("insert-sale")]
         public IActionResult SalePut(SaleData data)
         {
-            if (data == null)
+            if (data == null || data.CSaleH == null || data.CSaleD == null || data.CSalePay == null)
             {
-                return BadRequest("Los datos de venta o inventario son nulos.");
+                return BadRequest("Los datos de venta no pueden ser nulos.");
             }
             /*Declara variables*/
             JsonResult Response;
             bool Code;
-            string Message;
-            DataTable dt;
             int idFoliadorEntradaSalida = 5; // FOLIO DE ENTRADA SALIDA
-            int idFolioadorMovInv = 4; // FOLIO DE MOVIMIENTO INVETNARIO
+            int idFoliadorMovInv = 4; // FOLIO DE MOVIMIENTO INVETNARIO
             int idTipoMovInv = 1; //VENTA POR PUBLICO GENERAL
             string FolioEntradaSalida = "";
+            string Message;
             string FolioMovimiento = "";
-            SaleData saleData = data;
             
 
             //Referencias
-            DataBase2 db = new DataBase2();
-            MyToolsController tools = new MyToolsController();
-            InventoryController invmov = new InventoryController();
-            InventoryData inventoryData = new InventoryData();
-            InventoryH inventoryH = new InventoryH();
-
-            
-            SaleH? cSaleH = saleData.CSaleH;
-            List<SaleD>? cSaleD = saleData.CSaleD;
-            List<SalePay>? cSalePay = saleData.CSalePay;
-
-            InventoryH cMovInvH = new InventoryH();
-            List<InventoryD>? cMovInvD = new List<InventoryD>();
+            var db = new DataBase2();
+            var tools = new MyToolsController();
+            var invmov = new InventoryController();
+            var inventoryData = new InventoryData();
+            var cSaleH = data.CSaleH;
+            var cSaleD = data.CSaleD;
+            var cSalePay = data.CSalePay;
+            var cMovInvH = new InventoryH();
+            var cMovInvD = new List<InventoryD>();
             inventoryData.cDB = db;
 
             try
@@ -57,77 +49,24 @@ namespace MyApi.Controllers.Sale
                 db.BeginTransaction();
 
                 FolioEntradaSalida = tools.generatFolio(idFoliadorEntradaSalida, cSaleH.idEntidad, cSaleH.idUsuarioModifica, db);
-                FolioMovimiento = tools.generatFolio(idFolioadorMovInv, cSaleH.idEntidad, cSaleH.idUsuarioModifica, db);
+                FolioMovimiento = tools.generatFolio(idFoliadorMovInv, cSaleH.idEntidad, cSaleH.idUsuarioModifica, db);
                 cSaleH.folioEntradaSalida = Convert.ToInt32(FolioEntradaSalida);
                 cSaleH.folioMovimientoInventario = Convert.ToInt32(FolioMovimiento);
 
-                cMovInvH.idTipoMovimientoInventario = idTipoMovInv;
-                cMovInvH.folioMovimientoInventario = cSaleH.folioMovimientoInventario;
-                cMovInvH.idDocumentoReferencia = cSaleH.folioEntradaSalida;
-                cMovInvH.idAlmacen = cSaleH.idEntidad;
-                cMovInvH.idMotivoMovimiento = 2; //Baja por venta
-                cMovInvH.idEstadoMovimiento = 1; //Concluida
-                cMovInvH.idEstadoMovimiento = 1; //Concluida
-                cMovInvH.idPersona = 999; //Cliente genérico
-                cMovInvH.idEntidad = cSaleH.idEntidad;
-                cMovInvH.idUsuarioModifica = cSaleH.idUsuarioModifica;
-
-                inventoryData.CInventoryH = cMovInvH;
-
-                db.SetCommand("sp_in_entradasSalidas", true);
-                db.AddParameter("@folioEntradaSalida", cSaleH.folioEntradaSalida);
-                db.AddParameter("@folioMovimientoInventario", cSaleH.folioMovimientoInventario);
-                db.AddParameter("@idTipoEntradaSalida", cSaleH.idTipoEntradaSalida);
-                db.AddParameter("@comentarios", cSaleH.comentarios);
-                db.AddParameter("@activo", cSaleH.activo);
-                db.AddParameter("@idEntidad", cSaleH.idEntidad);
-                db.AddParameter("@idUsuarioModifica", cSaleH.idUsuarioModifica);
-                DataSet ds = db.ExecuteWithDataSet();
-                ds.Tables[0].TableName = "Data";
+                inventoryData.CInventoryH = CreateInventoryHeader(cSaleH,idTipoMovInv);
+                DataSet ds = ExecuteSaleHeader(db, cSaleH);
 
                 foreach (SaleD d in cSaleD) 
                 {
-
-                    db.SetCommand("sp_in_entradasSalidasDetalles", true);
-                    db.AddParameter("@folioEntradaSalida", cSaleH.folioEntradaSalida);
-                    db.AddParameter("@idProductoServicio", d.idProductoServicio);
-                    db.AddParameter("@cantidad", d.cantidad);
-                    db.AddParameter("@precioFinal", d.precioFinal);
-                    db.AddParameter("@idUnidadMedida", d.idUnidadMedida);
-                    db.AddParameter("@comentarios", d.comentarios);
-                    db.AddParameter("@activo", cSaleH.activo);
-                    db.AddParameter("@idEntidad", cSaleH.idEntidad);
-                    db.AddParameter("@idUsuarioModifica", cSaleH.idUsuarioModifica);
-                    db.Execute();
-
-                    InventoryD invD = new InventoryD();
-
-                    invD.idProductoServicio = d.idProductoServicio;
-                    invD.cantidad = d.cantidad;
-                    invD.idUnidadMedida = d.idUnidadMedida;
-                    invD.precioVentaUnitario = d.precioFinal;
-                    invD.comentarios = d.comentarios;
-                    invD.idEntidad = cSaleH.idEntidad;
-                    invD.idUsuarioModifica = cSaleH.idUsuarioModifica;
-                    cMovInvD.Add(invD);
+                    ExecuteSaleDetail(db,cSaleH,d);
+                    cMovInvD.Add(CreateInventoryDetail(cSaleH, d));
                 }
-
                 inventoryData.CInventoryD = cMovInvD;
 
                 foreach (SalePay p in cSalePay)
                 {
-                    db.SetCommand("sp_in_entradasSalidasPago", true);
-                    db.AddParameter("@folioEntradaSalida", cSaleH.folioEntradaSalida);
-                    db.AddParameter("@idTipoPago", p.idTipoPago);
-                    db.AddParameter("@montoPago", p.@montoPago);                    
-                    db.AddParameter("@numeroAutorizacion", p.numeroAutorizacion);
-                    db.AddParameter("@comentarios", p.comentarios);
-                    db.AddParameter("@activo", cSaleH.activo);
-                    db.AddParameter("@idEntidad", cSaleH.idEntidad);
-                    db.AddParameter("@idUsuarioModifica", cSaleH.idUsuarioModifica);
-                    db.Execute();
+                    ExecuteSalePayment(db,cSaleH,p);
                 }
-
 
                 invmov.InventoryMovementPutSale(inventoryData);
                 db.Commit();
@@ -145,6 +84,74 @@ namespace MyApi.Controllers.Sale
                 Response = MyToolsController.ToJson(Code, Message);
             }
             return Response;
+        }
+        private InventoryH CreateInventoryHeader(SaleH saleHeader, int idTipoMovInv)
+        {
+            return new InventoryH
+            {
+                idTipoMovimientoInventario = idTipoMovInv,
+                folioMovimientoInventario = saleHeader.folioMovimientoInventario,
+                idDocumentoReferencia = saleHeader.folioEntradaSalida,
+                idAlmacen = saleHeader.idEntidad,
+                idMotivoMovimiento = 2,
+                idEstadoMovimiento = 1,
+                idPersona = 999,
+                idEntidad = saleHeader.idEntidad,
+                idUsuarioModifica = saleHeader.idUsuarioModifica
+            };
+        }
+        private DataSet ExecuteSaleHeader(DataBase2 db, SaleH saleHeader)
+        {
+            db.SetCommand("sp_in_entradasSalidas", true);
+            db.AddParameter("@folioEntradaSalida", saleHeader.folioEntradaSalida);
+            db.AddParameter("@folioMovimientoInventario", saleHeader.folioMovimientoInventario);
+            db.AddParameter("@idTipoEntradaSalida", saleHeader.idTipoEntradaSalida);
+            db.AddParameter("@comentarios", saleHeader.comentarios);
+            db.AddParameter("@activo", saleHeader.activo);
+            db.AddParameter("@idEntidad", saleHeader.idEntidad);
+            db.AddParameter("@idUsuarioModifica", saleHeader.idUsuarioModifica);
+            DataSet ds = db.ExecuteWithDataSet();
+            return ds;
+        }
+        private void ExecuteSaleDetail(DataBase2 db, SaleH cSaleH , SaleD d)
+        {
+            db.SetCommand("sp_in_entradasSalidasDetalles", true);
+            db.AddParameter("@folioEntradaSalida", cSaleH.folioEntradaSalida);
+            db.AddParameter("@idProductoServicio", d.idProductoServicio);
+            db.AddParameter("@cantidad", d.cantidad);
+            db.AddParameter("@precioFinal", d.precioFinal);
+            db.AddParameter("@idUnidadMedida", d.idUnidadMedida);
+            db.AddParameter("@comentarios", d.comentarios);
+            db.AddParameter("@activo", cSaleH.activo);
+            db.AddParameter("@idEntidad", cSaleH.idEntidad);
+            db.AddParameter("@idUsuarioModifica", cSaleH.idUsuarioModifica);
+            db.Execute();
+        }
+        private InventoryD CreateInventoryDetail(SaleH saleHeader, SaleD saleDetail)
+        {
+            return new InventoryD
+            {
+                idProductoServicio = saleDetail.idProductoServicio,
+                cantidad = saleDetail.cantidad,
+                idUnidadMedida = saleDetail.idUnidadMedida,
+                precioVentaUnitario = saleDetail.precioFinal,
+                comentarios = saleDetail.comentarios,
+                idEntidad = saleHeader.idEntidad,
+                idUsuarioModifica = saleHeader.idUsuarioModifica
+            };
+        }
+        private void ExecuteSalePayment(DataBase2 db, SaleH saleHeader, SalePay salePay)
+        {
+            db.SetCommand("sp_in_entradasSalidasPago", true);
+            db.AddParameter("@folioEntradaSalida", saleHeader.folioEntradaSalida);
+            db.AddParameter("@idTipoPago", salePay.idTipoPago);
+            db.AddParameter("@montoPago", salePay.montoPago);
+            db.AddParameter("@numeroAutorizacion", salePay.numeroAutorizacion);
+            db.AddParameter("@comentarios", salePay.comentarios);
+            db.AddParameter("@activo", saleHeader.activo);
+            db.AddParameter("@idEntidad", saleHeader.idEntidad);
+            db.AddParameter("@idUsuarioModifica", saleHeader.idUsuarioModifica);
+            db.Execute();
         }
     }
 }
