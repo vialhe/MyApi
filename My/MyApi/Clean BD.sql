9f8cccf3f6c9055a23be079f4a473d7acf70bd23
Victor7u7 <107167883+vialhe@users.noreply.github.com>
2025-06-18 23:25:33 -0600

    Se agregan validaciones y registrso de pruebas
    Se agregan validaciones y registrso de pruebas

--- My/MyApi/Clean BD.sql
+++ My/MyApi/Clean BD.sql
@@ -1,6 +1,5 @@
 /*
 	 I M P O R T A N T E   :   L I M P I A R   R E G I S T R O S   D E   B D 
-/*
 
 Delete from proc_movimientosInventarios
 Delete from proc_movimientosInventariosDetalles
@@ -20,14 +19,12 @@ Delete From proc_corteTienda
 /*
 	 I M P O R T A N T E   :   A G R E G A R   C A T A L O G O S   A   C A D A   E N T I D A D 
 
-*/
+*//*
 SELECT * From cat_unidadesMedida WHERE idEntidad = 9999
 SELECT * From sys_folios
 SELECT * From sys_foliosContador
 GO
-SELECT * From sys_foliosContador
-
-go
+*/
 --exec sp_ui_catalogos
 --@id						= 0,
 --@descripcion			='Caja',
@@ -36,3 +33,26 @@ go
 --@activo					= 1,
 --@idUsuarioModifica		= 1,
 --@catalogo				= 'cat_unidadesMedida'
+
+/*
+
+	--------------------------------------------------------------------------------- 	 
+	--------------------------------------------------------------------------------- 	 
+
+*/
+
+
+Select * From inv_inventario
+Select * From inv_inventarioDet
+Select * From cat_productosServicios where id IN( 1159)--,1153,1161,1166)
+
+Select * From proc_movimientosInventarios where folioMovimientoInventario = 329
+Select * From proc_movimientosInventariosDetalles where folioMovimientoInventario = 329
+Select * From cat_tiposMovmientosInventario
+
+Select * From proc_entradasSalidas where folioEntradaSalida = 192
+Select * From proc_entradasSalidasDetalles where folioEntradaSalida = 192
+Select * From proc_entradasSalidaPago where folioEntradaSalida = 192
+Select * From cat_estadosEntradaSalida
+
+exec sp_se_corteTienda 9999
