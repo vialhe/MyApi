93fc69b7f8b50c3914bf4deee23608aa3675de9e
vial <107167883+vialhe@users.noreply.github.com>
2026-03-08 21:51:50 -0600

    Faltó subir la el modelo de Auth

--- My/MyApi/Models/Auth/Auth.cs
+++ My/MyApi/Models/Auth/Auth.cs
@@ -17,4 +17,48 @@ namespace MyApi.Class.Auth
 
     }
 
+    public class ExpiraSesionRequest
+    {
+        public int idEntidad { get; set; }
+    }
+
+    public class IniciarSesionUsuarioRequest
+    {
+        public int idEntidad { get; set; }
+        public int idUsuario { get; set; }
+        public string token { get; set; }
+        public string ip { get; set; }
+        public string userAgent { get; set; }
+        public string comentarios { get; set; }
+        public int idUsuarioAlta { get; set; }
+        public int minutosSesion { get; set; } = 5;
+    }
+
+    public class ValidaSesionUsuarioRequest
+    {
+        public int idEntidad { get; set; }
+        public string token { get; set; }
+    }
+
+    public class HeartbeatSesionUsuarioRequest
+    {
+        public int idEntidad { get; set; }
+        public string token { get; set; }
+        public int minutosSesion { get; set; } = 5;
+    }
+
+    public class CerrarSesionUsuarioRequest
+    {
+        public int idEntidad { get; set; }
+        public string token { get; set; }
+        public int? idUsuarioModifica { get; set; }
+        public string comentarios { get; set; }
+    }
+
+    public class ObtenerSesionActivaUsuarioRequest
+    {
+        public int idEntidad { get; set; }
+        public int idUsuario { get; set; }
+    }
+
 }
