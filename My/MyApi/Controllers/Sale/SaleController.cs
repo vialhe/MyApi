394459090c3ce51a35668ec37aff57f5e6b8382d
vial <107167883+vialhe@users.noreply.github.com>
2026-06-22 15:24:10 -0600

    Falto agregar archivo al commit

--- My/MyApi/Controllers/Sale/SaleController.cs
+++ My/MyApi/Controllers/Sale/SaleController.cs
@@ -207,7 +207,6 @@ namespace MyApi.Controllers.Sale
                 db.SetCommand("sp_se_corteCaja", true);
                 db.AddParameter("@idUsuarioIniciaCorte", data.idUsuarioIniciaCorte);
                 db.AddParameter("@idEntidad", data.idEntidad);
-                db.AddParameter("@idSucursal", data.idSucursal.HasValue ? data.idSucursal.Value : (object)DBNull.Value);
                 DataSet ds = db.ExecuteWithDataSet();
                 ds.Tables[0].TableName = "Data";
                 db.Close();
@@ -260,7 +259,6 @@ namespace MyApi.Controllers.Sale
                 db.AddParameter("@fechaFin", data.fechaFin);
                 db.AddParameter("@idEntidad", data.idEntidad);
                 db.AddParameter("@idEstadoTicket", data.idEstatusTicket);
-                db.AddParameter("@idSucursal", data.idSucursal.HasValue ? data.idSucursal.Value : (object)DBNull.Value);
                 DataSet ds = db.ExecuteWithDataSet();
                 db.Close();
 
@@ -547,8 +545,7 @@ namespace MyApi.Controllers.Sale
                 db.AddParameter("@fechaInicio", data.fechaInicio);
                 db.AddParameter("@fechaFin", data.fechaFin);
                 db.AddParameter("@idEntidad", data.idEntidad);
-                db.AddParameter("@idUsuario", data.idUsuario.HasValue ? data.idUsuario.Value : (object)DBNull.Value);
-                db.AddParameter("@idSucursal", data.idSucursal.HasValue ? data.idSucursal.Value : (object)DBNull.Value);
+                db.AddParameter("@idUsuario", data.idUsuario.HasValue ? data.idUsuario.Value : DBNull.Value);
                 DataSet ds = db.ExecuteWithDataSet();
                 ds.Tables[0].TableName = "Data";
                 ds.Tables[1].TableName = "DataD";
